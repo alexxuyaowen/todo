01aeb941312d45de05cc77e70a316b788f4da891
Yaowen Xu <43050345+alexxuyaowen@users.noreply.github.com>
2022-05-25 19:40:19 -0700

    +

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ fetch("https://jsonplaceholder.typicode.com/todos/")
 .then(res => res.json())
 .then(json => {
   const items = document.getElementById("items");
+  console.log(json);
 
-  json.forEach(e => items.innerHTML += e.completed ? `<p><s> ${e.id} ${e.title} </s></p>` : `<p> ${e.id} ${e.title} </p>`);
+  json.forEach(e => items.innerHTML += (e.completed ? `<p><span><s>${e.id} ${e.title}</s></span>` : `<span>${e.id} ${e.title}</span>`) + `&nbsp;<strong>User: ${e.userId}</strong></p>`);
 })
